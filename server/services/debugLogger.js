eb33be6671c7fb98e6b1de9a020f4abc9cdbca0a
GustavoResendizCast <124696432+GustavoResendizCast@users.noreply.github.com>
2023-10-17 00:49:30 +0000

    ⭐ Feature: Barra de navegación

--- server/services/debugLogger.js
+++ server/services/debugLogger.js
@@ -1,5 +1,5 @@
 // Importando dependencia debug
-import { Debug } from 'debug';
+import Debug from 'debug';
 // Establecemos el proceso debugger
 const debug = Debug('dwpc2:server');
 // Exportando el logger de debuggeo
