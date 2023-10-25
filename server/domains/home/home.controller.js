8c58c542426e8a15e5cc47747ac518d36b20f533
GustavoResendizCast <124696432+GustavoResendizCast@users.noreply.github.com>
2023-10-25 16:28:34 +0000

    ⭐ Feature: Agregando Vistas

--- server/domains/home/home.controller.js
+++ server/domains/home/home.controller.js
@@ -1,11 +1,19 @@
-// Action Methods
+// Actions methods
 // GET "/"
 // GET "/index"
 const home = (req, res) => {
-const iconSet = ['⭐', '🤖', '🍉'];
-    const icon = iconSet[Math.floor(Math.random() * 3)];
-    res.render('index', { title: 'DWPCII-2023A', icon });
-  };
-  export default {
-    home,
-  };
+  const iconSet = ['⭐', '🤖', '🍉', '📲', '🪸', '🌠', '🦾'];
+  const icon = iconSet[Math.floor(Math.random() * iconSet.length)];
+  res.render('home/homeView', { icon });
+};
+
+// GET "/about"
+const about = (req, res) => {
+  res.render('home/aboutView', { appVersion: '1.0.0' });
+};
+
+// Controlador Home
+export default {
+  home,
+  about,
+};
