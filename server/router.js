e4048b938407d941862b1cfc57a1e602a01c0cbf
GustavoResendizCast <124696432+GustavoResendizCast@users.noreply.github.com>
2023-12-06 02:01:40 +0000

    👮‍♀️ Feat: Autorizacion

--- server/router.js
+++ server/router.js
@@ -2,13 +2,16 @@
 import homeRouter from './domains/home/home.router';
 import userRouter from './domains/user/user.router';
 import userProject from './domains/project/project.router';
+
+// Importando el factory de autorizacion
+import AuthFactory from './services/authorizationFactory';
 // Función que agrega rutas
 const addRoutes = (app) => {
   // Agregando enrutado de Home
   app.use('/', homeRouter);
   app.use('/user', userRouter);
   app.use('/', userProject);
-  app.use('/project', userProject);
+  app.use('/project', AuthFactory('user'), userProject);
   return app;
 };
 
